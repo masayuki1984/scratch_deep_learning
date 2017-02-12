ebf9ce0139908d3c7f9d6cf29246342739709654
Masayuki Kato <masayuki_kato1984@icloud.com>
2017-02-13 00:07:48 +0900

    Added loss_function file.

---
+++ chapter_04/loss_functions.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# 二乗和誤差
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+# 交差エントロピー誤差
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
