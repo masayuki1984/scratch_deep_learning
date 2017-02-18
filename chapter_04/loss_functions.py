d8164f3734d63911d71c319745652e30bca0c039
Masayuki Kato <masayuki_kato1984@icloud.com>
2017-02-18 09:24:24 +0900

    finished chapter4-3.

--- chapter_04/loss_functions.py
+++ chapter_04/loss_functions.py
@@ -6,5 +6,10 @@ def mean_squared_error(y, t):
 
 # 交差エントロピー誤差
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y)) / batch_size
+
